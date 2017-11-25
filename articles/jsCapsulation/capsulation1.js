9778665832c67c524ee9cb161f8332d063602d2c
zhouxiong <zhouxiong03@meituan.com>
2017-11-25 13:55:27 +0800

    feat[jsCapsulation]: add jsCapsulation file

---
+++ articles/jsCapsulation/capsulation1.js
@@ -0,0 +1,205 @@
+/**
+ *
+ * @desc 判断两个数组是否相等
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Boolean}
+ */
+function arrayEqual(arr1, arr2) {
+    // 首先要判断是否是数组,传进来的非数组,返回false
+    if(!(arr1 instanceof Array) || !(arr2 instanceof Array)) {
+        return false;
+    }
+    if (arr1 === arr2) return true;
+    if (arr1.length != arr2.length) return false;
+    for (var i = 0; i < arr1.length; ++i) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+}
+
+/**
+ * @desc 获取浏览器类型和版本
+ * @return {String}
+ */
+function getExplore() {
+    var sys = {},
+        ua = navigator.userAgent.toLowerCase(),
+        s;
+    (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? sys.ie = s[1] :
+        (s = ua.match(/msie ([\d\.]+)/)) ? sys.ie = s[1] :
+            (s = ua.match(/edge\/([\d\.]+)/)) ? sys.edge = s[1] :
+                (s = ua.match(/firefox\/([\d\.]+)/)) ? sys.firefox = s[1] :
+                    (s = ua.match(/(?:opera|opr).([\d\.]+)/)) ? sys.opera = s[1] :
+                        (s = ua.match(/chrome\/([\d\.]+)/)) ? sys.chrome = s[1] :
+                            (s = ua.match(/version\/([\d\.]+).*safari/)) ? sys.safari = s[1] : 0;
+
+    // 根据关系进行判断
+    if (sys.ie) return ('IE: ' + sys.ie);
+    if (sys.edge) return ('EDGE: ' + sys.edge);
+    if (sys.firefox) return ('Firefox: ' + sys.firefox);
+    if (sys.chrome) return ('Chrome: ' + sys.chrome);
+    if (sys.opera) return ('Opera: ' + sys.opera);
+    if (sys.safari) return ('Safari: ' + sys.safari);
+    return 'Unkonwn';
+}
+
+/**
+ *
+ * @desc 获取操作系统类型
+ * @return {String}
+ */
+function getOS() {
+    var userAgent = 'navigator' in window
+            && 'userAgent' in navigator
+            && navigator.userAgent.toLowerCase() || '';
+    var vendor = 'navigator' in window
+            && 'vendor' in navigator
+            && navigator.vendor.toLowerCase() || '';
+    var appVersion = 'navigator' in window
+            && 'appVersion' in navigator
+            && navigator.appVersion.toLowerCase() || '';
+
+    if (/mac/i.test(appVersion))
+        return 'MacOSX';
+    if (/win/i.test(appVersion))
+        return 'windows';
+    if (/linux/i.test(appVersion))
+        return 'linux';
+    if (/iphone/i.test(userAgent) || /ipad/i.test(userAgent) || /ipod/i.test(userAgent))
+        return 'ios';
+    if (/android/i.test(userAgent))
+        return 'android';
+    if (/win/i.test(appVersion) && /phone/i.test(userAgent))
+        return 'windowsPhone';
+}
+
+/**
+ * @desc 深拷贝，支持常见类型
+ * @param {Any} values
+ */
+function deepClone(values) {
+    var copy;
+
+    // Handle the 3 simple types, and null or undefined
+    if (null == values || "object" != typeof values) return values;
+
+    // Handle Date
+    if (values instanceof Date) {
+        copy = new Date();
+        copy.setTime(values.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (values instanceof Array) {
+        copy = [];
+        for (var i = 0, len = values.length; i < len; i++) {
+            copy[i] = deepClone(values[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (values instanceof Object) {
+        copy = {};
+        for (var attr in values) {
+            if (values.hasOwnProperty(attr)) copy[attr] = deepClone(values[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy values! Its type isn't supported.");
+}
+
+/**
+ *
+ * @desc   判断`obj`是否为空
+ * @param  {Object} obj
+ * @return {Boolean}
+ */
+function isEmptyObject(obj) {
+    if (!obj || typeof obj !== 'object' || Array.isArray(obj))
+        return false
+    return !Object.keys(obj).length
+}
+
+/**
+ *
+ * @desc 生成指定范围随机数
+ * @param  {Number} min
+ * @param  {Number} max
+ * @return {Number}
+ */
+function randomNum(min, max) {
+    return Math.floor(min + Math.random() * (max - min));
+}
+
+/**
+ *
+ * @desc 随机生成颜色
+ * @return {String}
+ */
+function randomColor() {
+    return '#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).slice(-6);
+}
+
+/**
+ *
+ * @desc   现金额转大写
+ * @param  {Number} n
+ * @return {String}
+ */
+function digitUppercase(n) {
+    var fraction = ['角', '分'];
+    var digit = [
+        '零', '壹', '贰', '叁', '肆',
+        '伍', '陆', '柒', '捌', '玖'
+    ];
+    var unit = [
+        ['元', '万', '亿'],
+        ['', '拾', '佰', '仟']
+    ];
+    var head = n < 0 ? '欠' : '';
+    n = Math.abs(n);
+    var s = '';
+    for (var i = 0; i < fraction.length; i++) {
+        s += (digit[Math.floor(n * 10 * Math.pow(10, i)) % 10] + fraction[i]).replace(/零./, '');
+    }
+    s = s || '整';
+    n = Math.floor(n);
+    for (var i = 0; i < unit[0].length && n > 0; i++) {
+        var p = '';
+        for (var j = 0; j < unit[1].length && n > 0; j++) {
+            p = digit[n % 10] + unit[1][j] + p;
+            n = Math.floor(n / 10);
+        }
+        s = p.replace(/(零.)*零$/, '').replace(/^$/, '零') + unit[0][i] + s;
+    }
+    return head + s.replace(/(零.)*零元/, '元')
+            .replace(/(零.)+/g, '零')
+            .replace(/^整$/, '零元整');
+}
+
+/**
+ *
+ * @desc   判断是否为邮箱地址
+ * @param  {String}  str
+ * @return {Boolean}
+ */
+function isEmail(str) {
+    return /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(str);
+}
+
+/**
+ *
+ * @desc   判断是否为手机号
+ * @param  {String|Number} str
+ * @return {Boolean}
+ */
+function isPhoneNum(str) {
+    return /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/.test(str)
+}
+
+
+
